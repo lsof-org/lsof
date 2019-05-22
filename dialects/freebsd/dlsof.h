5496d4008732a88d82f8b60b0ada5d5481c8df28
Larry Rosenman <larryrtx@gmail.com>
2019-05-22 08:25:42 -0700

    support current

--- dialects/freebsd/dlsof.h
+++ dialects/freebsd/dlsof.h
@@ -45,6 +45,12 @@
 #include <signal.h>
 #include <unistd.h>
 
+#if    FREEBSDV>=13000
+/* This header is a huge mess.  Please don't support EOL FreeBSD releases. */
+#define        _KERNEL 1
+#include <sys/_lock.h>
+#undef _KERNEL
+#endif         /* FREEBSDV>=13000 */
 # if	FREEBSDV>=4000
 #  if	FREEBSDV>=5000
 #   if	FREEBSDV<6020
