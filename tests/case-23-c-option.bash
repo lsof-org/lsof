b5d1a4c3d4bcee213346b3d7e109d34483d4ddce
Jiajie Chen <c@jia.je>
2023-01-29 09:37:38 +0800

    Add tests for -c option

---
+++ tests/case-23-c-option.bash
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+source tests/common.bash
+
+file=$(mktemp /tmp/case-23-c-option.XXXXX)
+
+# lsof -clsof finds itself
+$lsof -n -clsof > $file &
+pid=$!
+wait $pid
+cat "$file" | grep "^lsof *${pid}" || exit 1
+
+# lsof -c^lsof excludes itself
+$lsof -n -c^lsof > $file &
+pid=$!
+wait $pid
+cat "$file" | grep "^lsof *${pid}" && exit 1
+
+# lsof -c/lsof/ includes itself
+$lsof -n -c/lsof/ > $file &
+pid=$!
+wait $pid
+cat "$file" | grep "^lsof *${pid}" || exit 1
+
+# try multiple filters
+$lsof -n -clsof -cbash -c/lsof/ > $file &
+pid=$!
+wait $pid
+cat "$file" | grep "^lsof *${pid}" || exit 1
+
+# terse output
+$lsof -n -clsof -t > $file &
+pid=$!
+wait $pid
+cat "$file" | grep "^${pid}$" || exit 1
